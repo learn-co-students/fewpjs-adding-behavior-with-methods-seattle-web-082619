a4d389c1ea151bdf3910788397f45a4674486c8e
Justin Maple <justinamaple@gmail.com>
2019-10-16 09:29:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+class Animal {
+  constructor(name, sex, sound='?') {
+    this.name = name;
+    this.sex = sex;
+    this.sound = sound;
+  }
+
+  speak() {
+    return `${this.name} says ${this.sound}!`;
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, sex, sound) {
+    super(name, sex, 'meow');
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, sex, sound) {
+    super(name, sex, 'woof');
+  }
+}
+
+class Bird extends Animal {
+  constructor(name, sex, sound) {
+    super(name, sex, 'squawk');
+  }
+
+  speak() {
+    return (this.sex === 'male') ? 
+      this.maleSpeak() : super.speak();
+  }
+
+  maleSpeak() {
+    return `It's me! ${this.name}, the parrot!`;
+  }
+}
